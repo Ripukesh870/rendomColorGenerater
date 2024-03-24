edda26edcae99cda3c098a9111917b6bf1bedaf9
Ripukesh <ripukesh870@gmail.com>
2024-03-24 22:14:26 +0530

    Describe your changes here

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Myapp from './component/Myapp'
 import RendomColor from './comp/RendomColor'
-import R1 from './comp/R1'
 
 export class App extends React.Component {
   render() {
     return (
       <>
         <RendomColor />
-        {/* <R1/> */}
       </>
 
       )
